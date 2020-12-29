c773bd64b9814d023c36f05d826f7c8d0f604ddf
ChamplainLeCode <champlainmarius20@gmail.com>
2020-12-29 11:20:37 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/etud.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/etud.dir/main.cpp.o"
+  "CMakeFiles/etud.dir/src/models/Student.cpp.o"
+  "etud"
+  "etud.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/etud.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
